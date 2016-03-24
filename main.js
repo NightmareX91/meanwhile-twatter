b02d37a1b75908c252df043eaa7cf5e6609f6391
meanwhile <nightmarex91@gmail.com>
2016-03-24 19:26:07 +0000

    Added authentication

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ catch (e) {
     console.log("Please run npm install and make sure it passes with no errors!");
     process.exit();
 }
+
+try {
+    var authDetails = require("./auth.json");
+}
+catch (e) {
+    console.log("You need to create an auth.json file using the example file on the github!");
+    process.exit();
+}
+
+var client = new twitter({
+    consumer_key: authDetails.consumer_key,
+    consumer_secret: authDetails.consumer_secret,
+    access_token_key: authDetails.access_token_key,
+    access_token_secret: authDetails.access_token_secret
+});
